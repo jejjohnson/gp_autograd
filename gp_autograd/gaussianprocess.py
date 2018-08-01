7a6a5f69a5b69acbd38b2f21a704bbfda171d5cb
Eman Johnson <emanjohnson91@gmail.com>
2018-08-01 21:44:37 +0200

    Added a new branch to work on the error.

--- gp_autograd/gaussianprocess.py
+++ gp_autograd/gaussianprocess.py
@@ -1,61 +1,150 @@
 import autograd.numpy as np
 import matplotlib.pyplot as plt
-from autograd import elementwise_grad as egrad
+from autograd import elementwise_grad as egrad, value_and_grad
 from scipy.linalg import solve_triangular
-from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import (ConstantKernel as C,
-                                              RBF, WhiteKernel)
+from scipy.optimize import minimize
+from sklearn.base import BaseEstimator, RegressorMixin
+
+from data import example_1d
 
 
-class GPAutoGrad(object):
-    """GPAutoGrad implements a GP Regression algorithm which utilizes
-    the autogradient to find the derivative of the mean function
-    and the derivative of the predictive variance function.
+# TODO: Draw Samples from Prior
+# TODO: Draw Samples from Posterior
+# Link: https://github.com/paraklas/GPTutorial/blob/master/code/gaussian_process.py
+# TODO: Generalize for any kernel methods
+# TODO: Add Comments
 
-    It inputs a trained model from the scikit-learn library using the
-    kernel function: C() * RBF() + WhiteKernel()
-    """
+# TODO: Constrained Optimization Function
+# TODO: Multiple Iterations for finding best weights for the derivative
+# Link: https://github.com/scikit-learn/scikit-learn/blob/f0ab589f/sklearn/gaussian_process/gpr.py#L451
 
-    def __init__(self, gp_model):
-        self.gp_model = gp_model
-        kernel_model = self.gp_model.kernel_
-        self.signal_variance = kernel_model.get_params()['k1__k1__constant_value']
-        self.length_scale = kernel_model.get_params()['k1__k2__length_scale']
-        self.likelihood_variance = kernel_model.get_params()['k2__noise_level']
-        self.weights = gp_model.alpha_
-        self.x_train = gp_model.X_train_
-        self.L = gp_model.L_
 
-    def fit(self):
-        return self
+class GaussianProcess(BaseEstimator, RegressorMixin):
+    def __init__(self, jitter=1e-8, random_state=None):
+        self.jitter = jitter
+        self.random_state = random_state
 
-    def predict(self, X):
+    def init_theta(self):
+        """Initializes the hyperparameters."""
+        signal_variance = 1.0
+        length_scale = np.ones(self.X_train_.shape[1])
+        noise_likelihood = 0.01
+        theta = np.array([signal_variance, noise_likelihood, length_scale])
+        return np.log(theta)
 
-        # kernel matrix
-        K = self.rbf_covariance(X, y=self.x_train,
-                                length_scale=self.length_scale,
-                                scale=self.signal_variance)
+    def fit(self, X, y):
 
-        return np.dot(K, self.weights)
+        self.X_train_ = X
+        self.y_train_ = y
 
-    def variance(self, X):
+        # initial hyper-parameters
+        theta0 = self.init_theta()
+
+        # minimize the objective function
+        best_params = minimize(value_and_grad(self.log_marginal_likelihood), theta0, jac=True,
+                               method='L-BFGS-B')
+        # Gather hyper parameters
+        signal_variance, noise_likelihood, length_scale = \
+            self._get_kernel_params(best_params.x)
+
+        self.signal_variance = np.exp(signal_variance)
+        self.noise_likelihood = np.exp(noise_likelihood)
+        self.length_scale = np.exp(length_scale)
+
+        # Calculate the weights
+        K = self.rbf_covariance(X, length_scale=self.length_scale,
+                                signal_variance=self.signal_variance)
+        K += self.noise_likelihood * np.eye(K.shape[0])
+        L = np.linalg.cholesky(K + self.jitter * np.eye(K.shape[0]))
+        weights = np.linalg.solve(L.T, np.linalg.solve(L, y))
+
+        self.weights = weights
+        self.L = L
+        self.K = K
 
         L_inv = solve_triangular(self.L.T, np.eye(self.L.shape[0]))
-        K_inv = np.dot(L_inv, L_inv.T)
 
-        K_trans = self.rbf_covariance(X, y=self.x_train,
+        self.K_inv = np.dot(L_inv, L_inv.T)
+
+        return self
+
+    def log_marginal_likelihood(self, theta):
+        x_train = self.X_train_
+        y_train = self.y_train_
+
+        if np.ndim == 1:
+            y_train = y_train[:, np.newaxis]
+
+        # Gather hyper parameters
+        signal_variance, noise_likelihood, length_scale = \
+            self._get_kernel_params(theta)
+        signal_variance = np.exp(signal_variance)
+        noise_likelihood = np.exp(noise_likelihood)
+        length_scale = np.exp(length_scale)
+
+        n_samples = x_train.shape[0]
+
+        # train kernel
+        K = self.rbf_covariance(x_train, length_scale=length_scale,
+                                signal_variance=signal_variance)
+        K += noise_likelihood * np.eye(n_samples)
+        L = np.linalg.cholesky(K + self.jitter * np.eye(n_samples))
+        weights = np.linalg.solve(L.T, np.linalg.solve(L, y_train))
+        log_likelihood_dims = -0.5 * np.einsum("ik,ik->k", y_train, weights)
+        log_likelihood_dims -= np.log(np.diag(L)).sum()
+        log_likelihood_dims -= (K.shape[0] / 2) * np.log(2 * np.pi)
+
+        log_likelihood = log_likelihood_dims.sum(-1)
+
+        return -log_likelihood
+
+    def predict(self, X, return_std=False):
+
+        # Train test kernel
+        K_trans = self.rbf_covariance(X, self.X_train_,
                                       length_scale=self.length_scale,
-                                      scale=self.signal_variance)
+                                      signal_variance=self.signal_variance)
+
+        pred_mean = np.dot(K_trans, self.weights)
+
+        if not return_std:
+            return pred_mean
+        else:
+            return pred_mean, self.variance(X, K_trans=K_trans)
+
+    def variance(self, X, K_trans=None):
+
+        if K_trans is None:
+            K_trans = self.rbf_covariance(X, y=self.X_train_,
+                                          length_scale=self.length_scale,
+                                          signal_variance=self.signal_variance)
 
         # compute the variance
         y_var = np.diag(self.rbf_covariance(X, length_scale=self.length_scale,
-                                            scale=self.signal_variance)) \
-                + self.likelihood_variance
-        y_var -= np.einsum("ij,ij->i", np.dot(K_trans, K_inv), K_trans)
+                                            signal_variance=self.signal_variance)) \
+                + self.noise_likelihood
+        y_var -= np.einsum("ij,ij->i", np.dot(K_trans, self.K_inv), K_trans)
 
         return y_var
 
-    def mu_grad(self, X, nder=1):
+    def _get_kernel_params(self, theta):
+
+        signal_variance = theta[0]
+        noise_likelihood = theta[1] + self.jitter
+        length_scale = theta[2:]
+
+        return signal_variance, noise_likelihood, length_scale
+
+    def rbf_covariance(self, X, y=None, signal_variance=1.0, length_scale=1.0):
+
+        if y is None:
+            y = X
+
+        D = np.expand_dims(X / length_scale, 1) - np.expand_dims(y / length_scale, 0)
+
+        return signal_variance * np.exp(-0.5 * np.sum(D ** 2, axis=2))
+
+    def mu_grad(self, X, nder=1, return_std=False):
 
         # Construct the autogradient function for the
         # predictive mean
@@ -63,10 +152,17 @@ class GPAutoGrad(object):
 
         if nder == 1:
             grad_mu = egrad(mu)
-            return grad_mu(X)
+
+            if not return_std:
+                return grad_mu(X)
+            else:
+                return grad_mu(X), self.sigma_grad(X, nder=1)
         else:
             grad_mu = egrad(egrad(mu))
-            return grad_mu(X)
+            if not return_std:
+                return grad_mu(X)
+            else:
+                return grad_mu(X), self.sigma_grad(X, nder=2)
 
     def sigma_grad(self, X, nder=1):
 
@@ -81,16 +177,212 @@ class GPAutoGrad(object):
             grad_var = egrad(egrad(sigma))
             return grad_var(X)
 
-    @staticmethod
-    def rbf_covariance(X, y=None, scale=1.0, length_scale=1.0):
+
+class GaussianProcessError(BaseEstimator, RegressorMixin):
+    def __init__(self, jitter=1e-8, x_covariance=None, random_state=None):
+        self.jitter = jitter
+        self.x_covariance = x_covariance
+        self.random_state = random_state
+
+    def init_theta(self):
+        """Initializes the hyperparameters."""
+        signal_variance = np.log(1.0)
+        length_scale = np.log(np.ones(self.X_train_.shape[1]))
+        noise_likelihood = np.log(0.01)
+
+        # Calculate the initial weights
+        K = self.rbf_covariance(self.X_train_, length_scale=np.exp(length_scale),
+                                signal_variance=np.exp(signal_variance))
+        K += np.exp(noise_likelihood) * np.eye(K.shape[0])
+        L = np.linalg.cholesky(K + self.jitter * np.eye(K.shape[0]))
+        init_weights = np.linalg.solve(L.T, np.linalg.solve(L, self.y_train_))
+        init_derivative = self.weights_grad(self.X_train_, init_weights,
+                                            np.exp(length_scale), np.exp(signal_variance))
+        init_derivative = init_derivative.ravel()
+
+        theta = np.hstack([signal_variance, noise_likelihood, length_scale, init_derivative])
+
+        return theta
+
+    def fit(self, X, y):
+
+        self.X_train_ = X
+        self.y_train_ = y
+
+        if self.x_covariance is None:
+            self.x_covariance = 0.0 * self.X_train_.shape[1]
+        if np.ndim(self.x_covariance) == 1:
+            self.x_covariance = np.array([self.x_covariance])
+
+        # initial hyper-parameters
+        theta0 = self.init_theta()
+
+        # minimize the objective function
+        best_params = minimize(value_and_grad(self.log_marginal_likelihood), theta0, jac=True,
+                               method='L-BFGS-B')
+        print(best_params)
+        # Gather hyper parameters
+        signal_variance, noise_likelihood, length_scale, init_weights = \
+            self._get_kernel_params(best_params.x)
+
+        self.signal_variance = np.exp(signal_variance)
+        self.noise_likelihood = np.exp(noise_likelihood)
+        self.length_scale = np.exp(length_scale)
+
+        # Calculate the weights
+        K = self.rbf_covariance(X, length_scale=self.length_scale,
+                                signal_variance=self.signal_variance)
+        K += self.noise_likelihood * np.eye(K.shape[0])
+        L = np.linalg.cholesky(K + self.jitter * np.eye(K.shape[0]))
+        weights = np.linalg.solve(L.T, np.linalg.solve(L, y))
+
+        self.weights = weights
+        self.L = L
+        self.K = K
+
+        L_inv = solve_triangular(self.L.T, np.eye(self.L.shape[0]))
+
+        self.K_inv = np.dot(L_inv, L_inv.T)
+
+        return self
+
+    def log_marginal_likelihood(self, theta):
+        x_train = self.X_train_
+        y_train = self.y_train_
+
+        if np.ndim == 1:
+            y_train = y_train[:, np.newaxis]
+
+        # Gather hyper parameters
+        signal_variance, noise_likelihood, length_scale, init_derivative = \
+            self._get_kernel_params(theta)
+        signal_variance = np.exp(signal_variance)
+        noise_likelihood = np.exp(noise_likelihood)
+        length_scale = np.exp(length_scale)
+
+        # Calculate the derivative
+        derivative = np.reshape(init_derivative, (self.X_train_.shape[0], self.X_train_.shape[1]))
+        derivative_term = np.diag(np.einsum("ij,ij->i", np.dot(derivative, self.x_covariance), derivative))
+
+        n_samples = x_train.shape[0]
+
+        # Calculate derivative of the function
+
+        # train kernel
+        K = self.rbf_covariance(x_train, length_scale=length_scale,
+                                signal_variance=signal_variance)
+        K += noise_likelihood * np.eye(n_samples)
+        K += derivative_term
+        L = np.linalg.cholesky(K + self.jitter * np.eye(n_samples))
+        weights = np.linalg.solve(L.T, np.linalg.solve(L, y_train))
+        log_likelihood_dims = -0.5 * np.einsum("ik,ik->k", y_train, weights)
+        log_likelihood_dims -= np.log(np.diag(L)).sum()
+        log_likelihood_dims -= (K.shape[0] / 2) * np.log(2 * np.pi)
+
+        log_likelihood = log_likelihood_dims.sum(-1)
+
+        return -log_likelihood
+
+    def predict(self, X, return_std=False):
+
+        # Train test kernel
+        K_trans = self.rbf_covariance(X, self.X_train_,
+                                      length_scale=self.length_scale,
+                                      signal_variance=self.signal_variance)
+
+        pred_mean = np.dot(K_trans, self.weights)
+
+        if not return_std:
+            return pred_mean
+        else:
+            return pred_mean, self.variance(X, K_trans=K_trans)
+
+    def variance(self, X, K_trans=None):
+
+        if K_trans is None:
+            K_trans = self.rbf_covariance(X, y=self.X_train_,
+                                          length_scale=self.length_scale,
+                                          signal_variance=self.signal_variance)
+
+        # compute the variance
+        y_var = np.diag(self.rbf_covariance(X, length_scale=self.length_scale,
+                                            signal_variance=self.signal_variance)) \
+                + self.noise_likelihood
+        y_var -= np.einsum("ij,ij->i", np.dot(K_trans, self.K_inv), K_trans)
+
+        return y_var
+
+    def _get_kernel_params(self, theta):
+
+        signal_variance = theta[0]
+        noise_likelihood = theta[1] + self.jitter
+        length_scale = theta[2:self.X_train_.shape[1] + 2]
+        init_weights = theta[self.X_train_.shape[1] + 1:-1]
+        # print(length_scale.shape, init_weights.shape)
+
+        return signal_variance, noise_likelihood, length_scale, init_weights
+
+    def rbf_covariance(self, X, y=None, signal_variance=1.0, length_scale=1.0):
+
         if y is None:
             y = X
-        D = np.expand_dims(X / length_scale, 1) \
-            - np.expand_dims(y / length_scale, 0)
-        return scale * np.exp(-0.5 * np.sum(D ** 2, axis=2))
+
+        D = np.expand_dims(X / length_scale, 1) - np.expand_dims(y / length_scale, 0)
+
+        return signal_variance * np.exp(-0.5 * np.sum(D ** 2, axis=2))
+
+    def predict_weights(self, X, weights, length_scale, signal_variance):
+        # Train test kernel
+        K_trans = self.rbf_covariance(X,
+                                      length_scale=length_scale,
+                                      signal_variance=signal_variance)
+
+        pred = np.dot(K_trans, weights)
+
+        return pred
+
+    def weights_grad(self, X, weights, length_scale, signal_variance):
+
+        mu = lambda x: self.predict_weights(x, weights, length_scale, signal_variance)
+
+        grad_mu = egrad(mu)
+        return grad_mu(X)
+
+    def mu_grad(self, X, nder=1, return_std=False):
+
+        # Construct the autogradient function for the
+        # predictive mean
+        mu = lambda x: self.predict(x)
+
+        if nder == 1:
+            grad_mu = egrad(mu)
+
+            if not return_std:
+                return grad_mu(X)
+            else:
+                return grad_mu(X), self.sigma_grad(X, nder=1)
+        else:
+            grad_mu = egrad(egrad(mu))
+            if not return_std:
+                return grad_mu(X)
+            else:
+                return grad_mu(X), self.sigma_grad(X, nder=2)
+
+    def sigma_grad(self, X, nder=1):
+
+        # Construct the autogradient function for the
+        # predictive variance
+        sigma = lambda x: self.variance(x)
+
+        if nder == 1:
+            grad_var = egrad(sigma)
+            return grad_var(X)
+        else:
+            grad_var = egrad(egrad(sigma))
+            return grad_var(X)
 
 
-def np_gradient(y_pred, xt, n_points=1000):
+def np_gradient(y_pred, xt):
     return np.gradient(y_pred.squeeze(), xt.squeeze(), edge_order=2)[:, np.newaxis]
 
 
@@ -117,37 +409,38 @@ def sample_data():
 
 
 def main():
+    test_error()
+    pass
+
+
+def test_error():
+    X, y, error_params = example_1d()
+    # Initialize GP Model
+    gp_autograd = GaussianProcessError()
+
+    # Fit GP Model
+    gp_autograd.fit(X['train'], y['train'])
+    pass
+
+
+def test_original():
     # Get sample data
     xtrain, xtest, ytrain, ytest = sample_data()
 
-    # experimental parameters
-    seed = 123
-    signal_variance = 1.0
-    length_scale = 1.0
-    noise_likelihood = 0.01
-
-    # Kernel Parameters
-    gp_kernel = C(signal_variance) \
-                * RBF(length_scale) \
-                + WhiteKernel(noise_likelihood)
-
     # Initialize GP Model
-    gp_model = GaussianProcessRegressor(kernel=gp_kernel, random_state=seed)
+    gp_autograd = GaussianProcess()
 
-    gp_model.fit(xtrain, ytrain)
+    # Fit GP Model
+    gp_autograd.fit(xtrain, ytrain)
 
-    # Get predictions
-    y_pred, y_var = gp_model.predict(xtest, return_std=True)
+    # Make Predictions
+    y_pred, y_var = gp_autograd.predict(xtest, return_std=True)
 
-    # Extract new kernel parameters
-    signal_variance = gp_model.kernel_.get_params()['k1__k1__constant_value']
-    length_scale = gp_model.kernel_.get_params()['k1__k2__length_scale']
-    noise_level = gp_model.kernel_.get_params()['k2__noise_level']
 
     ##########################
     # First Derivative
     ##########################
-    gp_autograd = GPAutoGrad(gp_model)
+
 
     # Autogradient
     mu_der = gp_autograd.mu_grad(xtest)
@@ -174,8 +467,6 @@ def main():
     mu_der2 = gp_autograd.mu_grad(xtest, nder=2)
     num_grad2 = np_gradient(num_grad, xtest)
 
-    assert (mu_der2.all() == num_grad2.all())
-
     # Plot
     fig, ax = plt.subplots(figsize=(10, 7))
 
@@ -189,8 +480,9 @@ def main():
 
     plt.show()
 
-    return None
+    assert (mu_der2.all() == num_grad2.all())
 
+    return None
 
 if __name__ == '__main__':
     main()
